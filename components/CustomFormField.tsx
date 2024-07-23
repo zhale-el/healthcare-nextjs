cfeb5c1249a9196553b01cc6c993ea9907c73688
zhaleh <zhalehelmi@gmail.com>
2024-07-23 18:36:38 +0330

    add form

---
+++ components/CustomFormField.tsx
@@ -0,0 +1,30 @@
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const CustomFormField = () => {
+  return (
+    <FormField
+      control={form.control}
+      name="username"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Username</FormLabel>
+          <FormControl>
+            <Input placeholder="shadcn" {...field} />
+          </FormControl>
+          <FormDescription>This is your public display name.</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default CustomFormField;
